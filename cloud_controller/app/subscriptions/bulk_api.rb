994c04fbbfc6469411bbe99e63a9fe7663631d02
Bob Nugmanov <bnugmanov@vmware.com>
2012-07-18 19:34:09 -0700

    WIP: cc_partition
    Change-Id: I8f406264ffc3d01dd067d12ce18e6676b1a8e1df

--- cloud_controller/app/subscriptions/bulk_api.rb
+++ cloud_controller/app/subscriptions/bulk_api.rb
@@ -1,5 +1,5 @@
 EM.next_tick do
-  NATS.subscribe('cloudcontroller.bulk.credentials') do |_, reply|
+  NATS.subscribe("cloudcontroller.bulk.credentials.#{AppConfig[:cc_partition]}") do |_, reply|
     NATS.publish(reply, AppConfig[:bulk_api][:auth].to_json)
   end
 end
