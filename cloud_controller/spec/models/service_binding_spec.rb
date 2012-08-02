bdfd70bf189b188d1f84f0a1290f41adbe610497
Andrew Liu <aliu@vmware.com>
2012-08-02 15:39:57 +0800

    [CC] fix label info for multi versions service
    Change-Id: Ie7a4778cb7839ff20c93cef99cc526851bb44a93

--- cloud_controller/spec/models/service_binding_spec.rb
+++ cloud_controller/spec/models/service_binding_spec.rb
@@ -54,4 +54,41 @@ describe ServiceBinding do
     bdg.save
     bdg.should_not be_valid
   end
+
+  describe '#for_staging' do
+    it "should show correct label for multi versions service" do
+      u = User.new(:email => 'foo@bar.com')
+      u.set_and_encrypt_password('foobar')
+      u.save
+      u.should be_valid
+
+      a = App.new(
+        :owner     => u,
+        :name      => 'foobar',
+        :framework => 'sinatra',
+        :runtime   => 'ruby18'
+      )
+      a.save
+      a.should be_valid
+
+      svc = Service.new(:label => "foo-1.0", :url=>"http://example.com", :token => 'bar')
+      svc.save
+      svc.should be_valid
+
+      cfg = ServiceConfig.new(:name => 'foo', :alias => 'bar', :service => svc,
+                          :data => {"version" => "2.0"})
+      cfg.save
+      cfg.should be_valid
+
+      binding = ServiceBinding.new(
+        :name => 'foo',
+        :app  => a,
+        :service_config => cfg
+      )
+      binding.save
+      binding.should be_valid
+
+      binding.for_staging[:label].should == "foo-2.0"
+    end
+  end
 end
